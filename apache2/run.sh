6d8dfa429c99a2b56d4145585cd1084e03709136
Fabian Seitz <seitzf1@yahoo.de>
2020-05-18 10:25:03 +0200

    Fixed SSL integration &removed MariaDB integration

--- apache2/run.sh
+++ apache2/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bashio
 ssl=$(bashio::config 'ssl')
-mariadb_filename=$(bashio::config 'mariadb_filename')
-mariadb_username=$(bashio::config 'mariadb_username')
-mariadb_username=$(bashio::config 'mariadb_username')
+website_name=$(bashio::config 'website_name')
+certfile=$(bashio::config 'certfile')
+keyfile=$(bashio::config 'keyfile')
 
 if [ -d /var/www/localhost/htdocs/ ]; then
     rm -r /var/www/localhost/htdocs/
@@ -12,50 +12,38 @@ fi
 
 if [ $ssl = "true" ]; then
     echo "You have activated SSL. SSL Settings will be applied"
-    a2enmod ssl
+    mkdir /etc/apache2/sites-enabled
+    sed -i '/LoadModule rewrite_module/s/^#//g' /etc/apache2/httpd.conf
+    echo "Include /etc/apache2/sites-enabled/*.conf" >> /etc/apache2/httpd.conf
+    echo "<VirtualHost *:80>" > /etc/apache2/sites-enabled/000-default.conf
+    echo "ServerName $website_name"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "ServerAdmin webmaster@localhost"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "DocumentRoot /var/www/localhost/htdocs/"  >> /etc/apache2/sites-enabled/000-default.conf
+
+    echo "#Redirect http to https"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "    RewriteEngine On"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "    RewriteCond %{HTTPS} off"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "    RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "#End Redirect http to https"  >> /etc/apache2/sites-enabled/000-default.conf
+
+    echo "    ErrorLog /var/log/error.log"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "        #CustomLog /var/log/access.log combined"  >> /etc/apache2/sites-enabled/000-default.conf
+    echo "</VirtualHost>"  >> /etc/apache2/sites-enabled/000-default.conf
+
+    echo "<IfModule mod_ssl.c>"  > /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "<VirtualHost *:443>"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "ServerName $website_name"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "ServerAdmin webmaster@localhost"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "DocumentRoot /var/www/localhost/htdocs/"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+
+    echo "    ErrorLog /var/log/error.log"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "        #CustomLog /var/log/access.log combined"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "SSLCertificateFile /ssl/$certfile"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "SSLCertificateKeyFile /ssl/$keyfile"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "</VirtualHost>"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
+    echo "</IfModule>"  >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
 else
     echo "SSL is deactivated"
-fi
-
-#if bashio::config.has_value 'mariadb_filename'; then
-if [ $mariadb_filename != "off" ]; then
-    echo "mariadb file was given, preparing mariadb"
-    if ! bashio::services.available 'mysql'; then
-        bashio::log.fatal \
-        "Local database access should be provided by the MariaDB addon"
-        bashio::exit.nok \
-        "Please ensure it is installed and started"
-    fi
-    host=$(bashio::services "mysql" "host")
-    password=$(bashio::services "mysql" "password")
-    port=$(bashio::services "mysql" "port")
-    username=$(bashio::services "mysql" "username")
-
-    bashio::log.warning "Using the Maria DB addon"
-    bashio::log.warning "Please ensure this is included in your backups"
-    bashio::log.warning "Uninstalling the MariaDB addon will remove any data"
-
-    bashio::log.info "Creating database $mariadb_filename on MariaDB Addon $host if required"
-    mysql \
-        -u "${username}" -p"${password}" \
-        -h "${host}" -P "${port}" \
-        -e "CREATE DATABASE IF NOT EXISTS \`$mariadb_filename\` ;"
-    if [ -f /share/$mariadb_filename.sql ]; then
-    bashio::log.info "Listing current Tables of $mariadb_filename Database with user ${username}"
-    mysql \
-        -u "${username}" -p"${password}" \
-        -h "${host}" -P "${port}" \
-        -e "SHOW TABLES FROM $mariadb_filename ;"
-    bashio::log.info "Importing file  /share/$mariadb_filename.sql on MariaDB Addon $host with user ${username}"
-    mysql \
-        -u "${username}" -p"${password}" \
-        -h "${host}" -P "${port}" \
-        -e "--database=$mariadb_filename < /share/$mariadb_filename.sql ;"
-    else
-    bashio::log.warning "File /share/$mariadb_filename.sql not found! Skipping MariaDB import."
-    fi
-else
-    echo "no mariadb file was given. Mariadb wont be set up."
 fi
 
 echo "Here is your web file architecture."
