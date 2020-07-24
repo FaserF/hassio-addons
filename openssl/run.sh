89b0ef2e1341655f95e7dbb74be02684afca6a48
Fabian Seitz <seitzf1@yahoo.de>
2020-07-24 11:01:59 +0200

    Initial Upload of the new OpenSSL addon

---
+++ openssl/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bashio
+website_name=$(bashio::config 'website_name')
+key_file=/ssl/key_openssl.pem
+cert_file=/ssl/cert_openssl.pem
+
+if test -f "$key_file"; then
+    echo "$key_file exists already. A new one will now be created!"
+    rm $key_file
+fi
+
+if test -f "$cert_file"; then
+    echo "$cert_file exists already. A new one will now be created!"
+    rm $cert_file
+fi
+
+openssl req -x509 -newkey rsa:4096 -keyout $key_file -out $cert_file -days 10000 -nodes -subj /CN=$website_name
+
+echo "Certificates were generated. They are now located here: $key_file & $cert_file . The addon will now be stopped."
+exit
