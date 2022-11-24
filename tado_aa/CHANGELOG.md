6896e9b1e00434f8662dd99f8f91c555c0a3d9b5
Fabian Seitz <fabian.seitz@pari.com>
2022-11-24 19:39:10 +0100

    fix username/password overwriting

--- tado_aa/CHANGELOG.md
+++ tado_aa/CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 1.0.1
+- Implement new username password detection from base python source
+
 ## 1.0.0
 - Initial release
--- tado_aa/run.sh
+++ tado_aa/run.sh
@@ -2,8 +2,8 @@
 username=$(bashio::config 'username')
 password=$(bashio::config 'password')
 
-sed -i "s/your_username@mail.com/${username}/" tado_aa.py
-sed -i "s/your_password/${password}/" tado_aa.py
+sed -i "s/your_tado_username/${username}/" tado_aa.py
+sed -i "s/your_tado_password/${password}/" tado_aa.py
 
 echo "Starting Tado Auto Assist python script from adrianslabu/tado_aa"
 python3 tado_aa.py
