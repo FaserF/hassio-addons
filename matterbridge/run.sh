be5df75062e313d55ce455a4f9394e138072240a
Fabian Seitz <fabian.seitz@pari.com>
2022-12-16 23:35:07 +0100

    improved matterbridge

--- matterbridge/run.sh
+++ matterbridge/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 config_path=$(bashio::config 'config_path')
+log_level=$(bashio::config 'log_level')
 
 if ! [ -f $config_path ]; then
 	echo "There is no $config_path file! Please create one for your needs! See here for an example: https://github.com/42wim/matterbridge/blob/master/matterbridge.toml.sample"
@@ -9,9 +10,12 @@ fi
 echo "Found config file at $config_path . Copying it now."
 cp $config_path /etc/matterbridge/matterbridge.toml
 
+parameter="-conf /etc/matterbridge/matterbridge.toml"
+if [ $log_level = "debug" ]; then
+	parameter="${parameter} -debug"
+fi
 echo "" > /var/log/matterbridge.log
 
 echo "Starting Matterbridge..."
-exec /bin/matterbridge -conf /etc/matterbridge/matterbridge.toml
-sleep 3
+exec /bin/matterbridge ${parameter} &
 exec tail -f /var/log/matterbridge.log
