faba8b42d221993dcfb36926c1aab7c75850ad25
Adrian Knorr <adrian.knorr@consol.de>
2024-11-15 15:20:09 +0100

    snclient not running

--- snclient/run.sh
+++ snclient/run.sh
@@ -17,5 +17,6 @@ log_level=$(bashio::config 'log_level')
 # echo "" >/var/log/matterbridge.log
 
 echo "Starting SNClient..."
-exec snclient daemon
+exec snclient 
+echo "SNClien started
 exec tail -f /var/log/snclient/snclient.log
