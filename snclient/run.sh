2ceb1607c453def5301629bc830f47144d993fd2
Adrian Knorr <adrian.knorr@consol.de>
2024-11-15 14:35:29 +0100

    Add SNClient addon

---
+++ snclient/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bashio
+config_path=$(bashio::config 'config_path')
+log_level=$(bashio::config 'log_level')
+
+# if ! [ -f $config_path ]; then
+# 	echo "There is no $config_path file! Please create one for your needs! See here for an example: https://github.com/42wim/matterbridge/blob/master/matterbridge.toml.sample"
+# 	echo "Try restarting the addon once your config file has been fully configured an been put somewhere on the HA /share folder."
+# 	exit 1
+# fi
+# echo "Found config file at $config_path . Copying it now."
+# cp $config_path /etc/matterbridge/matterbridge.toml
+
+# parameter="-conf /etc/matterbridge/matterbridge.toml"
+# if [ $log_level = "debug" ]; then
+# 	parameter="${parameter} -debug"
+# fi
+# echo "" >/var/log/matterbridge.log
+
+echo "Starting SNClient..."
+exec snclient daemon
+exec tail -f /var/log/snclient/snclient.log
