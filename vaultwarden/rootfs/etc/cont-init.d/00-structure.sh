3149df87d9b1d1dc440e9ff5a92160ff671b92e9
Fabian Seitz <github@fabiseitz.de>
2026-01-28 12:31:01 +0100

    another try for startup fixes

--- vaultwarden/rootfs/etc/cont-init.d/00-structure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Community Add-on: Vaultwarden
-# Ensure s6 environment structure exists before services start
-# ==============================================================================
-mkdir -p /var/run/s6/container_environment
