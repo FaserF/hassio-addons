0a49ed2ecad77270efb8fd3352f1121ceb843dd3
FaserF <12374134+FaserF@users.noreply.github.com>
2026-04-24 09:31:35 +0000

    style: auto-fix (black,isort,prettier,markdownlint)

--- .scripts/bump_version.py
+++ .scripts/bump_version.py
@@ -20,8 +20,9 @@ from datetime import datetime
 if sys.platform == "win32":
     try:
         import io
-        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-        sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+        sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
     except Exception:
         pass
 
